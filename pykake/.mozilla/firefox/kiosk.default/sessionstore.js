51f4bf76f236695923587623d1e4171f6e464be5
aviau <alexandre.viau@savoirfairelinux.com>
2014-10-09 11:47:16 -0400

    Added browser reload

---
+++ pykake/.mozilla/firefox/kiosk.default/sessionstore.js
@@ -0,0 +1,1 @@
+{"windows":[],"selectedWindow":0,"_closedWindows":[],"session":{"lastUpdate":1411666409796,"startTime":1411666406384,"recentCrashes":0},"scratchpads":[],"global":{}}
--- pykake/app.py
+++ pykake/app.py
@@ -2,28 +2,53 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import json
+
+import config
+
+import os
+import subprocess
+import shutil
+import time
 
 app = Flask(__name__)
 
-with open('config.json', 'rb') as fp:
-    config = json.load(fp)
+pykake_dir = os.path.dirname(os.path.abspath(__file__))
+cfg = config.load_config(pykake_dir)
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', config=config)
+    return render_template('index.html', config=cfg)
+
 
 @app.route('/', methods=['POST'])
 def post():
-    config['tabs'] = filter(None, request.form.getlist('option[]'))
-    save_config()
+    cfg['tabs'] = filter(None, request.form.getlist('option[]'))
+    config.save_config(pykake_dir, cfg)
+    reload_browser()
     return "New tabs saved"
 
-def save_config():
-    # Save the new config to file
-    with open('config.json', 'wb') as fp:
-        json.dump(config, fp)
+
+def reload_browser():
+    # Kill the browser
+    os.system("pkill -9 -f firefox")
+
+    # Reset the browser config
+    shutil.rmtree("/home/pi/.mozilla", ignore_errors=True)
+    shutil.copytree(
+        os.path.join(pykake_dir, '.mozilla'),
+        "/home/pi/.mozilla"
+    )
+
+    # Launch the browser
+    for tab in cfg['tabs']:
+        subprocess.Popen(
+            'firefox -new-tab ' + tab,
+            shell=True, stdin=None, stdout=None, stderr=None
+        )
+        time.sleep(2)
 
 
 if __name__ == '__main__':
+    reload_browser()
     app.run(debug=True)
